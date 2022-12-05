01c2b67cd1116f883346578d1c7b04f6c6bfa2e2
bassil <bassiljarrah2@gmail.com>
2022-12-05 22:31:56 +0300

    2.1 update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ const Home=()=>{
   return(
     <div className="home">
       <Routes> 
-       <Route path="bassilmohamad.github.io/my-netflix/" exact element={<LandingPage />}/>
+       <Route path="https://bassilmohamad.github.io/my-netflix/" exact element={<LandingPage />}/>
        <Route path="/movies" exact element={<Movies />}/>
        <Route path="/moviesDetail" exact element={<MoviesDetail />}/>
       </Routes>
